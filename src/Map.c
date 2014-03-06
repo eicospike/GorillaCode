767ff54cc8ad2a9955eeeec3a964d5cff4206300
eicospike <eicospike@sdf.org>
2014-03-06 04:46:45 +0000

    soundboard

--- src/Map.c
+++ src/Map.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	size = st.st_size;
 	printf("size%isize",size);
 	numint = size / sizeof(int);	
-	printf("ints%iints",numint);
+	printf("ints%iints\n",numint);
 
 //new crap end
     fd = open(filename, O_RDWR);
@@ -51,12 +51,22 @@ int main(int argc, char *argv[])
     printf("%i\n",result);
 //it now detects file size and maps it to memory, needs tested with all sizes and types of file
     map = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    for (i = 1; i <=numint; ++i) {
-//        map[i] = 0x4d6e6e61;
-	map[i] = 0x8;
-	i = i + 1;
+    printf("%x\n",map[0]);
+    for (i = 25; i <=numint; ++i) {
+    	map[i] = 0x00000000;
     }
-    printf("%x",map[111]);
+    for (i = 25; i <=numint; ++i) {
+//        map[i] = 0x4d6e6e61;
+//	map[i] = 0x12345678;
+	map[i] = 0xF0F0F0F0;
+//	map[i] = map[0];
+	i = i + 200;
+    }
+    for (i = 25; i <=numint; i++) {
+    	map[i] = 0x0F0F0F0F;
+	i = i + 401;
+    }
+    printf("%x\n",map[2]);
     munmap(map, size);
     close(fd);
     return 0;
