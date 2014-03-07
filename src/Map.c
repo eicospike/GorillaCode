4070d3ececb3835b3681c38b4760f78b0eb934cd
eicospike <eicospike@sdf.org>
2014-03-07 06:32:15 +0000

    eicogorilla

--- src/Map.c
+++ src/Map.c
@@ -27,91 +27,39 @@ int main(int argc, char *argv[])
     struct stat st;
     double param, results;
 
-//some directory mobility start    
     	dirname = "./";
        dp = opendir (dirname);
        if (dp != NULL)
          {
            while (ep = readdir (dp))
              puts (ep->d_name);
-//	     fputs (0, ep->d_type);
            (void) closedir (dp);
          }
        else
          perror ("Couldn't open the directory");    
-    
     filename = "data/nnapped.bin";
-//new crap begin
 	stat(filename, &st);
 	size = st.st_size;
 	printf("size%isize",size);
 	numint = size / sizeof(int);	
 	printf("ints%iints\n",numint);
-
-//new crap end
     fd = open(filename, O_RDWR);
     result = lseek(fd, size-1, SEEK_SET);
     result = write(fd, "", 1);
     result = lseek(fd, 0L, SEEK_END);
     printf("%i\n",result);
-//it now detects file size and maps it to memory, needs tested with all sizes and types of file
     map = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     printf("%x\n",map[0]);
     for (i = 25; i <=numint; ++i) {
     	map[i] = 0x00000000;
     }
-/*
-//for (I =  0; I <=numint; ++I) {
-    for (i = 25; i <=numint; i++) {
-//        map[i] = 0x4d6e6e61;
-//	map[i] = 0x12345678;
-	map[i] = 0xFFFFFFFF;
-//	map[i] = map[0];
-	i = i + 200;
-    }
-//    I = I + 4;
-//}
-//k = 1;
-//for (I =  0; I <=numint; I++) {
-    for (i = 25; i <=numint; i++) {
-    	map[i] = 0x0F0F0F0F;
-	k = 1;
-	for (j = 1; j <=10; j++) {
-	map[i+j] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-	k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-	map[i+j+k] = 0xFFFFFFFF;
-        k = k + 1;
-//	}
-//
-//	i = 101 + i;
-//    }
-//    I = I + 4;
-//}
-*/
+//###################################
+//############TONE BEEP##############
+//###################################
+//###################################
+//############TONE BEEP##############
+//###################################
+
 I = 0x00110011;
 for (k = 1; k <= 12; k++){
 
@@ -146,9 +94,6 @@ printf("%dsintest\n",result);
 //I = result * j;
 for (i = 1; i <= abs(result); i++){
 I = I - j * result / abs(result);
-
-
-
 }
 printf("%08Xhexchange\n",I);
 map[a + A] = I;
@@ -157,19 +102,11 @@ a = a + 1;
 A = a + A;
 }
 //}
-
-
-
-
-
-
-
-
+//###################################
+//############TONE BEEP##############
+//###################################
 j = 0x01010101;
 I = 0xAAAAAAAA;
-a = 25;
-
-
 a = 1;
 for (b = 1; b <=100; b++){
 for (k = 1; k <=360; k++){
@@ -179,129 +116,129 @@ param = 0.1 + k;
 results = sin(param *PI/180) * 100;
 result = (int) results;
 printf("%dsintest\n",result);
-//I = I * sin(param *PI/180);
-//sprintf(result, "%06X", (int)I & 0xffffff);
-//n=sprintf (buffer, "%08X", (int)I & 0xffffffff);
-//printf("%ssprint\n",buffer);
-//I = result * j;
 for (i = 1; i <= abs(result); i++){
 I = I - j * result / abs(result);
-
-
-
 }
 printf("%08Xhexchange\n",I);
 map[a + A] = I;
 a = a + 1;
 }
-
 }
-
+//###################################
+//############TONE BEEP##############
+//###################################
 j = 0x01010101;
 I = 0xAAAAAAAA;
-a = 25;
-
-
 a = 1;
 for (b = 1; b <=100; b++){
 for (k = 1; k <=360; k++){
-k = k + 15;
+k = k + 10;
 I = 0x00000000;
 param = 0.1 + k;
 results = sin(param *PI/180) * 100;
 result = (int) results;
 printf("%dsintest\n",result);
-//I = I * sin(param *PI/180);
-//sprintf(result, "%06X", (int)I & 0xffffff);
-//n=sprintf (buffer, "%08X", (int)I & 0xffffffff);
-//printf("%ssprint\n",buffer);
-//I = result * j;
 for (i = 1; i <= abs(result); i++){
 I = I - j * result / abs(result);
-
-
-
 }
 printf("%08Xhexchange\n",I);
 map[a + A] = I;
 a = a + 1;
 }
-
 }
 A = A + a;
-
+//###################################
+//############TONE BEEP##############
+//###################################
 j = 0x01010101;
 I = 0xAAAAAAAA;
 a = 25;
-
-
 a = 1;
 for (b = 1; b <=100; b++){
 for (k = 1; k <=360; k++){
-k = k + 15;
+k = k + 5;
 I = 0x00000000;
 param = 0.1 + k;
 results = sin(param *PI/180) * 100;
 result = (int) results;
 printf("%dsintest\n",result);
-//I = I * sin(param *PI/180);
-//sprintf(result, "%06X", (int)I & 0xffffff);
-//n=sprintf (buffer, "%08X", (int)I & 0xffffffff);
-//printf("%ssprint\n",buffer);
-//I = result * j;
 for (i = 1; i <= abs(result); i++){
 I = I - j * result / abs(result);
-
-
-
 }
 printf("%08Xhexchange\n",I);
 map[a + A] = I;
 a = a + 1;
 }
-
 }
 A = A + a;
 j = 0x01010101;
 I = 0xAAAAAAAA;
 a = 25;
-
-
+//###################################
+//############TONE BEEP##############
+//###################################
 a = 1;
 for (b = 1; b <=100; b++){
 for (k = 1; k <=360; k++){
-k = k + 15;
+k = k + 2;
 I = 0x00000000;
 param = 0.1 + k;
 results = sin(param *PI/180) * 100;
 result = (int) results;
 printf("%dsintest\n",result);
-//I = I * sin(param *PI/180);
-//sprintf(result, "%06X", (int)I & 0xffffff);
-//n=sprintf (buffer, "%08X", (int)I & 0xffffffff);
-//printf("%ssprint\n",buffer);
-//I = result * j;
 for (i = 1; i <= abs(result); i++){
 I = I - j * result / abs(result);
-
-
-
 }
 printf("%08Xhexchange\n",I);
 map[a + A] = I;
 a = a + 1;
 }
-
 }
-
-//for (i = 25; i <= 39; i++) {
-//        j = 0x00001100;
-//        I = I + j;
-//        printf("%x\n",I);
-//}
-
-
-
+A = A + a;
+//###################################
+//############TONE BEEP##############
+//###################################
+a = 1;
+for (b = 1; b <=100; b++){
+for (k = 1; k <=360; k++){
+k = k + 1;
+I = 0x00000000;
+param = 0.1 + k;
+results = sin(param *PI/180) * 100;
+result = (int) results;
+printf("%dsintest\n",result);
+for (i = 1; i <= abs(result); i++){
+I = I - j * result / abs(result);
+}
+printf("%08Xhexchange\n",I);
+map[a + A] = I;
+a = a + 1;
+}
+}
+A = A + a;
+//###################################
+//############TONE BEEP##############
+//###################################
+a = 1;
+for (b = 1; b <=100; b++){
+for (k = 1; k <=360; k++){
+k = k + 3;
+I = 0x00000000;
+param = 0.1 + k;
+results = sin(param *PI/180) * 100;
+result = (int) results;
+printf("%dsintest\n",result);
+for (i = 1; i <= abs(result); i++){
+I = I - j * result / abs(result);
+}
+printf("%08Xhexchange\n",I);
+map[a + A] = I;
+a = a + 1;
+}
+}
+//###################################
+//############TONE BEEP##############
+//###################################
     printf("%x\n",map[0]);
     munmap(map, size);
     close(fd);
